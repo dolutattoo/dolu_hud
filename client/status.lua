ff2302f84bf90910f783a3d0bd4774dd58b9a403
Dolu <alfrider02@gmail.com>
2022-10-22 05:02:43 +0200

    fix(client): send item use to nui

--- client/status.lua
+++ client/status.lua
@@ -1,21 +1,15 @@
--- Sending data from server to NUI
-RegisterNetEvent('ox:setPlayerData', function(name, data)
-	if player?.loaded and Config.status[name] then
-		SendNUIMessage({
-			action = 'setStatusValue',
-			data = {
-				statusName = name,
-				value = data
-			}
-		})
-	end
-	playerStatus[name] = data
-end)
-
+-- Receive event from ox_inventory, when a satus item is used
 RegisterNetEvent('ox:status:update', function(name, value)
 	value = utils.percent(value, 1000000, 2) -- Because people use 1000000 as max value
 	local currentStatus = playerStatus[name]
 	local newStatus = utils.formatPercentage(value > 0 and currentStatus + value or currentStatus - value)
+	SendNUIMessage({
+		action = 'setStatusValue',
+		data = {
+			statusName = name,
+			value = newStatus
+		}
+	})
 	playerStatus[name] = newStatus
 	utils.debug(1, "^7Action:add:^5" .. name, "^7Value added:^5" .. value, "^7New status:^5" .. newStatus .. "^7")
 end)
