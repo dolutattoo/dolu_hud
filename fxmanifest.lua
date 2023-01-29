c1ed78b0f5383be7410079c548059ebbd1b44b7f
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2023-01-29 14:45:29 +0000

    chore: bump manifest version to v1.3.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ game 'gta5'
 
 --[[ Resource Information ]] --
 name 'dolu_hud'
-version      '1.2.0'
+version      '1.3.0'
 description 'HUD for overextended framework'
 author 'Dolu'
 repository 'https://github.com/dolutattoo/dolu_hud'
