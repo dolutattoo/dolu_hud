17a7e283cb35e5b9ebec402fb515373974afcd64
Dolu <alfrider02@gmail.com>
2023-01-29 02:51:54 +0100

    refactor(client/seatbelt): better code

--- client/seatbelt.lua
+++ client/seatbelt.lua
@@ -1,35 +1,22 @@
 local isBuckled = false
-
-local Buckled = function ()
-    CreateThread(function ()
-        while isBuckled do
-            DisableControlAction(0, 75, true)
-            Wait(0)
-        end
-    end)
-end
-
-SetFlyThroughWindscreenParams(15.0, 20.0, 17.0, 2000.0)
-local Seatbelt = function (status)
-    if status then
-        SendNUIMessage({action = 'setSeatbelt', data = true})
-        SetFlyThroughWindscreenParams(1000.0,1000.0,0.0,0.0)
-        Buckled()
-    else
-        SendNUIMessage({action = 'setSeatbelt', data = false})
-        SetFlyThroughWindscreenParams(15.0, 20.0, 17.0, 2000.0)
-    end
-    isBuckled = status
-end
-
 local curInVehicle
 
-CreateThread(function ()
+local function updateSeatbeltHud(state)
+	SendNUIMessage({action = 'setSeatbelt', data = state})
+	if state then
+		SetFlyThroughWindscreenParams(1000.0,1000.0,0.0,0.0)
+	else
+		SetFlyThroughWindscreenParams(15.0, 20.0, 17.0, 2000.0)
+	end
+end
+
+CreateThread(function()
+	updateSeatbeltHud(false)
     while true do
         if nuiReady then
             local inVehicle = cache.vehicle
             if inVehicle ~= curInVehicle then
-                SendNUIMessage({action = 'setSeatbelt', data = isBuckled})
+                updateSeatbeltHud(isBuckled)
                 if not inVehicle and isBuckled then isBuckled = false end
                 curInVehicle = inVehicle
             end
@@ -38,14 +25,22 @@ CreateThread(function ()
     end
 end)
 
-RegisterCommand('dolu_hud:seatbelt', function ()
+RegisterCommand('dolu_hud:seatbelt', function()
     if cache.vehicle then
         local curVehicleClass = GetVehicleClass(cache.vehicle)
 
-        if curVehicleClass ~= 8
-        and curVehicleClass ~= 13
-        and curVehicleClass ~= 14
-        then Seatbelt(not isBuckled) end
+        if curVehicleClass ~= 8 and curVehicleClass ~= 13 and curVehicleClass ~= 14 then
+			if not isBuckled then
+				updateSeatbeltHud(true)
+				repeat
+					DisableControlAction(0, 75, true)
+					Wait(0)
+				until not isBuckled
+			else
+				updateSeatbeltHud(false)
+			end
+			isBuckled = not isBuckled
+		end
     end
 end, false)
 RegisterKeyMapping('dolu_hud:seatbelt', 'Toggle seatbelt', 'keyboard', Config.seatbeltKey)
