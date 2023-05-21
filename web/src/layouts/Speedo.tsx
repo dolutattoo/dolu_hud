3b191cd580e9f5dfeed9e6a9205327d745d933f6
Kacpherek <kacperpapiez255@gmail.com>
2023-05-21 03:14:02 +0200

    Update Speedo component to improve animation and performance
    This pull request aims to optimize the speedometer component by reducing the frequency of FiveM natives and introducing a smooth speed animation.
    
    Instead of sending a NUIMessage every 20ms, the speedometer component now updates at higher intervals without compromising the visual appearance much.
    
    Also the values can be tweaked to create more smooth animation.

--- web/src/layouts/Speedo.tsx
+++ web/src/layouts/Speedo.tsx
@@ -1,122 +1,201 @@
-import React, { useState } from 'react'
-import { Box, Center, Progress, RingProgress, Text, ThemeIcon } from '@mantine/core'
-import { useNuiEvent } from '../hooks/useNuiEvent'
-import { BiGasPump , BiBattery} from 'react-icons/bi'
-import seatbeltIcon from '../img/seatbelt.svg'
-import config from '../../../config.json'
-
+import React, { useState, useEffect } from "react";
+import { Box, Center, Progress, RingProgress, Text, ThemeIcon } from "@mantine/core";
+import { useNuiEvent } from "../hooks/useNuiEvent";
+import { BiGasPump, BiBattery } from "react-icons/bi";
+import seatbeltIcon from "../img/seatbelt.svg";
+import config from "../../../config.json";
 
 interface Speedo {
-    speed: number
-    rpm: number
-    fuelLevel: number
-    electric: boolean
+  speed: number;
+  rpm: number;
+  fuelLevel: number;
+  electric: boolean;
 }
 
 const Speedo: React.FC = () => {
-    // Visibility
-    const [visible, setVisible] = useState<boolean>(false)
-        useNuiEvent('toggleSpeedo', (state: boolean) => setVisible(state))
+  const [visible, setVisible] = useState<boolean>(false);
+  const [seatbeltColor, setSeatbeltColor] = useState<string>("rgba(200, 0, 0, 0.7)");
+  const [speed, setSpeed] = useState<number>(0);
+  const [rpm, setRpm] = useState<number>(0);
+  const [fuelLevel, setFuelLevel] = useState<number>(0);
+  const [fuelLevelColor, setFuelLevelColor] = useState<string>("red");
+  const [electric, setElectric] = useState<boolean>(false);
 
-    // Seatbelt
-    const [seatbeltColor, setSeatbeltColor] = useState<string>('rgba(200, 0, 0, 0.7)')
-        useNuiEvent('setSeatbelt', (state: boolean) =>
-        setSeatbeltColor(state === true ? 'rgba(200, 200, 200, 0.9)' : 'rgba(200, 0, 0, 0.7)')
-    )
+  const getColor = (value: number, color: string): string => (value > 10 ? color : "orange");
 
-    // ProgressBars states values
-    const [speed, setSpeed] = useState<number>(0)
-    const [rpm, setRpm] = useState<number>(0)
-    const [fuelLevel, setFuelLevel] = useState<number>(0)
-    const [fuelLevelColor, setFuelLevelColor] = useState<string>('red')
-    const [electric, setElectric] = useState<boolean>(false)
+  const animateSpeed = (targetSpeed: number, duration: number) => {
+    const startSpeed = speed;
+    const increment = (targetSpeed - startSpeed) / (duration / 10);
+    let currentSpeed = startSpeed;
+  
+    const easeOutQuad = (t: number) => t * (2 - t);
 
-    const getColor = (value: number, color:string) => {
-        if (value > 10) { return color } else { return 'orange' }
-    }
-
-    // Set values from client script
-    useNuiEvent('setSpeedo', (data: Speedo) => {
-        if (!visible) setVisible(true)
-        setSpeed(data.speed)
-        setRpm((data.rpm*100)/1)
-        setFuelLevel(data.fuelLevel)
-        setFuelLevelColor(getColor(data.fuelLevel, 'gray.4'))
-        setElectric(data.electric)
-    })
-
-    const getRpmColor = (value: number) => {
-        if (value < 45) {
-            return 'teal'
-        } else if (value < 60) {
-            return 'green'
-        } else if (value < 85) {
-            return 'yellow'
-        } else if (value < 95) {
-            return 'orange'
+    const updateSpeed = () => {
+      if (increment > 0) {
+        currentSpeed += increment;
+        if (currentSpeed < targetSpeed) {
+          const progress = currentSpeed / targetSpeed;
+          const easedProgress = easeOutQuad(progress);
+          const easedSpeed = startSpeed + (targetSpeed - startSpeed) * easedProgress;
+          setSpeed(Math.round(easedSpeed));
+          requestAnimationFrame(updateSpeed);
         } else {
-            return 'red'
+          setSpeed(targetSpeed);
         }
+      } else {
+        currentSpeed -= increment;
+        if (currentSpeed > targetSpeed) {
+          const progress = (startSpeed - currentSpeed) / (startSpeed - targetSpeed);
+          const easedProgress = easeOutQuad(progress);
+          const easedSpeed = startSpeed - (startSpeed - targetSpeed) * easedProgress;
+          setSpeed(Math.round(easedSpeed));
+          requestAnimationFrame(updateSpeed);
+        } else {
+          setSpeed(targetSpeed);
+        }
+      }
+    };
+        
+    updateSpeed();
+  };
+
+  const animateRpm = (targetRpm: number, duration: number) => {
+    const startRpm = rpm;
+    const increment = (targetRpm - startRpm) / (duration / 10);
+    let currentRpm = startRpm;
+
+    const updateRpm = () => {
+      currentRpm += increment;
+      setRpm(Math.round(currentRpm));
+
+      if ((increment > 0 && currentRpm < targetRpm) || (increment < 0 && currentRpm > targetRpm)) {
+        requestAnimationFrame(updateRpm);
+      }
+    };
+
+    updateRpm();
+  };
+
+  useNuiEvent("toggleSpeedo", (state: boolean) => setVisible(state));
+  useNuiEvent("setSeatbelt", (state: boolean) =>
+    setSeatbeltColor(state ? "rgba(200, 200, 200, 0.9)" : "rgba(200, 0, 0, 0.7)")
+  );
+  useNuiEvent("setSpeedo", (data: Speedo) => {
+    if (!visible) setVisible(true);
+
+    animateSpeed(data.speed, 400);
+    animateRpm((data.rpm * 100) / 1, 500);
+
+    setFuelLevel(data.fuelLevel);
+    setFuelLevelColor(getColor(data.fuelLevel, "gray.4"));
+    setElectric(data.electric);
+  });
+
+  const getRpmColor = (value: number): string => {
+    const lookupTable: { [key: number]: string } = {
+      0: "teal",
+      45: "green",
+      60: "yellow",
+      85: "orange",
+      95: "red",
+    };
+
+    const sortedKeys = Object.keys(lookupTable)
+      .map(Number)
+      .sort((a, b) => a - b);
+
+    for (let i = 0; i < sortedKeys.length; i++) {
+      const key = sortedKeys[i];
+      if (value < key) {
+        return lookupTable[key];
+      }
     }
 
-    return (
+    return "red";
+  };
+
+  useEffect(() => {
+    const speedoMetrics = config.speedoMetrics === "kmh" ? "Km/h" : "Mph";
+    const fuelLevelColor = getColor(fuelLevel, "gray.4");
+
+    setFuelLevelColor(fuelLevelColor);
+  }, [config.speedoMetrics, fuelLevel]);
+
+  return (
+    <>
+      {visible && (
         <>
-        {visible && <>
-            <Center>
-                <Box style={{ position: 'fixed', bottom: '7vh', width: '200px', height: '80px', marginTop: '-10vh', backgroundColor: 'rgba(0, 0, 0, 0.5)', borderRadius: '0.7vh', justifyContent: 'center' }}>
-                    {/* RPM */}
-                    <Progress
-                        value={rpm}
-                        color={getRpmColor(rpm)}
-                        style={{ margin: '7px' }}
-                    />
+          <Center>
+            <Box
+              style={{
+                position: "fixed",
+                bottom: "7vh",
+                width: "200px",
+                height: "80px",
+                marginTop: "-10vh",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                borderRadius: "0.7vh",
+                justifyContent: "center",
+              }}
+            >
+              <Progress value={rpm} color={getRpmColor(rpm)} style={{ margin: "7px" }} />
 
-                    {/* SEATBELT */}
-                    <div style={{ position: 'relative', margin: '5px', float: 'left' }}>
-                        <img
-                            src={seatbeltIcon}
-                            alt="seatbeltIcon"
-                            style={{ margin: '5px', marginTop: '7px', width: '30px', fill: seatbeltColor, backgroundColor: seatbeltColor, borderRadius: '7px' }}
-                        />
-                    </div>
+              <div style={{ position: "relative", margin: "5px", float: "left" }}>
+                <img
+                  src={seatbeltIcon}
+                  alt="seatbeltIcon"
+                  style={{
+                    margin: "5px",
+                    marginTop: "7px",
+                    width: "30px",
+                    fill: seatbeltColor,
+                    backgroundColor: seatbeltColor,
+                    borderRadius: "7px",
+                  }}
+                />
+              </div>
 
-                    {/* SPEED */}
-                    <div style={{ position: 'absolute', marginLeft: '80px' }}>
-                        <Center>
-                            <Text
-                                color='gray.4'
-                                size={20}
-                                weight={800}
-                                style={{ marginTop: '-2px' }}
-                            >{speed}</Text>
-                        </Center>
-                        <Center>
-                            <Text
-                                color='gray.4'
-                                size='sm'
-                                weight={800}
-                                style={{ marginBottom: '-10px' }}
-                            >{config.speedoMetrics === 'kmh' ? 'Km/h' : 'Mph'}</Text>
-                        </Center>
-                    </div>
+              <div style={{ position: "absolute", marginLeft: "80px" }}>
+                <Center>
+                  <Text color="gray.4" size={20} weight={800} style={{ marginTop: "-2px" }}>
+                    {speed}
+                  </Text>
+                </Center>
+                <Center>
+                  <Text color="gray.4" size="sm" weight={800} style={{ marginBottom: "-10px" }}>
+                    {config.speedoMetrics === "kmh" ? "Km/h" : "Mph"}
+                  </Text>
+                </Center>
+              </div>
 
-                    {/* FUEL */}
-                    <div style={{ position: 'relative', margin: '5px', float: 'right' }}>
-                      {fuelLevel !== undefined && <RingProgress sections={[{ value: fuelLevel, color: fuelLevelColor }]} thickness={6/1.2} size={55/1.2} roundCaps
-                          label={
-                            <Center>
-                                <ThemeIcon color={fuelLevelColor} variant='light' radius='xl' size={44/1.2}>
-                                  {electric ? <BiBattery size={23} /> : <BiGasPump size={23} />}
-                                </ThemeIcon>
-                            </Center>
-                        }
-                      />}
-                    </div>
-                </Box>
-            </Center>
-            </>}
+              <div style={{ position: "relative", margin: "5px", float: "right" }}>
+                {fuelLevel !== undefined && (
+                  <RingProgress
+                    sections={[{ value: fuelLevel, color: fuelLevelColor }]}
+                    thickness={6 / 1.2}
+                    size={55 / 1.2}
+                    roundCaps
+                    label={
+                      <Center>
+                        <ThemeIcon
+                          color={fuelLevelColor}
+                          variant="light"
+                          radius="xl"
+                          size={44 / 1.2}
+                        >
+                          {electric ? <BiBattery size={23} /> : <BiGasPump size={23} />}
+                        </ThemeIcon>
+                      </Center>
+                    }
+                  />
+                )}
+              </div>
+            </Box>
+          </Center>
         </>
-        )
-    }
+      )}
+    </>
+  );
+};
 
-export default Speedo
+export default Speedo;
